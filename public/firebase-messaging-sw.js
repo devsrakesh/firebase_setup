b482eb496bf46d4f9c235d52f9d86c582cafdd33
devsrakesh <developer.rakesh.rajput@gmail.com>
2023-10-29 23:49:15 +0530

    fix:

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken } from "firebase/messaging";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import and configure the Firebase SDK
+// These scripts are made available when the app is served or deployed on Firebase Hosting
+// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCIshYXUNtTk5Va8EMHZlfr27h86FwYU84",
   authDomain: "engage-598a6.firebaseapp.com",
@@ -14,27 +14,20 @@ const firebaseConfig = {
   appId: "1:177065416317:web:134599fbd7cdffdbda8314",
   measurementId: "G-PR9PLLYQPS",
 };
-
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-messaging.onBackgroundMessage((payload) => {
-  const { title, body } = payload.notification;
+messaging.onBackgroundMessage(function (payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
   const notificationOptions = {
-    body,
-    icon: "", // Replace with the path to your notification icon
+    body: "Background Message body.",
+    icon: "/vite.svg",
   };
 
-  self.registration.showNotification(title, notificationOptions);
-
-  // You can also trigger in-app notifications here
-  // For example, you can use the Broadcast Channel API to communicate with your app
-  const channel = new BroadcastChannel("notification-channel");
-  channel.postMessage(payload);
-});
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-  // event.waitUntil(clients.openWindow("YOUR_REDIRECT_URL"));
-  // Replace YOUR_REDIRECT_URL with the URL you want to open when the notification is clicked.
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
